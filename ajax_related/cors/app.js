4656f9c9a32e94bdddcfe4bc49c5491e4ba0271e
byronlun <2664181855@qq.com>
2016-09-09 14:20:17 +0800

    jsonp cors

---
+++ ajax_related/cors/app.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var server = http.createServer();
+server.on('request', onRequest);
+server.listen(3030);
+
+function onRequest (req, res) {
+	var pathname = url.parse(req.url).pathname;
+	if(pathname === '/') {
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+			'Access-Control-Allow-Origin': '*'
+		});
+		fs.readFile('index.html', 'utf8', (err, data) => {
+			if(err) {
+				console.log(err);
+			}
+			res.write(data);
+			res.end();
+		});
+	}
+}
---
+++ ajax_related/jsonp/app.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer();
+server.on('request', onRequest);
+server.listen(3030);
+
+var data = {'name': 'byronlun', 'purpose': 'test for jsonp'};
+
+function onRequest (req, res) {
+	var params = url.parse(req.url, true);
+	console.log(params);
+	console.log(params.query.callback);
+	if(params.query && params.query.callback) {
+		var str  = params.query.callback + '(' + JSON.stringify(data) + ')'; //jsonp
+		res.end(str);
+	}
+}
