00de2eb93703ba49c3b7973e140c57484d7e2bc8
byronlun <2664181855@qq.com>
2016-09-13 22:53:53 +0800

    just some commonly usage

--- react-router/demo/index.js
+++ react-router/demo/index.js
@@ -1,53 +1,90 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {Router, Route, IndexRoute, Link, browserHistory} from 'react-router';
+import {Router, Route, IndexRoute, Link, IndexLink, browserHistory, Lifecycle} from 'react-router';
 
-class Main extends Component {
+
+class MainLayout extends Component{
   render() {
     return (
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/users">Users</Link></li>
-        <li><Link to="/widgets">Widgets</Link></li>
-      </ul> 
+      <div className="app">
+        <aside className="primary-aside">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/inputsave">InputSave</Link></li>
+          </ul>
+        </aside>
+        <main>
+          {this.props.children}
+        </main>
+      </div>
     )
   }
 }
 
-class Home extends Component {
-	render() {
-		return (<h1>Welcome to the Home Page</h1>)
-	}
-}
-
-class Users extends Component {
+class Home extends Component{
   render() {
-    return (
-      <ul>
-        <li>user</li>
-        <li>user</li>
-        <li>user</li>
-      </ul>
-    )
+    return (<h1>Home Page</h1>)
   }
 }
 
-class Widgets extends Component {
+class UserList extends Component{
   render() {
     return (
-      <ul>
-        <li>widget</li>
-        <li>widget</li>
-        <li>widget</li>
+      <ul className="user-list">
+        <li>byronlun</li>
+        <li>xiaoyu</li>
+        <li>ben</li>
       </ul>
+      )
+  }
+}
+
+class InputSave extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      save: true
+    };
+    //Member methods are not "auto-bound" in ES6 classes
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+  }
+  
+  // 不知道怎么用~要用到再看把
+  routerWillLeave(nextLocation) {
+      if (!this.state.save) {
+      console.log('你的内容尚未保存,确定离开?');
+      return '你的内容尚未保存,确定离开?'
+    }
+  }
+
+  handleChange() {
+    this.setState({save: false});
+    console.log(this.state.save);
+  }
+  handleSave() {
+    this.setState({save: true});
+    console.log(this.state.save);
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.handleChange} />
+        <button onClick={this.handleSave}>save</button>
+      </div>
     )
   }
 }
 
 render((
-	<Router history={browserHistory}>
-    <Route path="/" component={Main} />
-      <Route path="/users" component={Users} />
-      <Route path="/widgets" component={Widgets} />
-	</Router>
+  <Router history={browserHistory}>
+    <Route path="/" component={MainLayout}>
+      <IndexRoute component={Home} />
+      <Route path="users" component={UserList} onEnter={
+        () => {console.log('可以利用onenter做验证或者替代redirect')}
+      }/>
+      <Route path="inputsave" component={InputSave} />
+    </Route>
+  </Router>
 ), document.getElementById('root'))
