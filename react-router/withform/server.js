05170a80bdff0d188e01b2afc29fa995b6e8d436
byronlun <2664181855@qq.com>
2016-09-15 23:57:39 +0800

    server router

--- react-router/withform/server.js
+++ react-router/withform/server.js
@@ -6,12 +6,55 @@ var app = express();
 // must be first!
 // app.use(compression());
 
+
+// import some new stuff
+import React from 'react'
+// we'll use this to render our app to an html string
+import { renderToString } from 'react-dom/server'
+// and these to match the url to routes and then render
+import { match, RouterContext } from 'react-router'
+import routes from './modules/routes'
+
+
+
 //设置静态文件目录
 app.use(express.static(__dirname));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+   // match the routes to the url
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    // in here we can make some decisions all at once
+    if (err) {
+      // there was an error somewhere during route matching
+      res.status(500).send(err.message)
+    } else if (redirect) {
+      // we haven't talked about `onEnter` hooks on routes, but before a
+      // route is entered, it can redirect. Here we handle on the server.
+      res.redirect(redirect.pathname + redirect.search)
+    } else if (props) {
+      // if we got props then we matched a route and can render
+      const appHtml = renderToString(<RouterContext {...props}/>)
+      res.send(renderPage(appHtml))
+    } else {
+      // no errors, no redirect, we just didn't match anything
+      res.status(404).send('Not Found')
+    }
+  })
+
+  // res.sendFile(path.join(__dirname, 'index.html'));
 })
+
+function renderPage(appHtml) {
+  return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8/>
+    <title>My First React Router App</title>
+    <div id=app>${appHtml}</div>
+    <script src="/build/bundle.js"></script>
+   `
+}
+
 
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
