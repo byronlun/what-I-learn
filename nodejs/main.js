78feae55028c07cbd30fe01c53960126c1cfddbe
byronlun <2664181855@qq.com>
2017-01-22 18:35:17 +0800

    爬虫 node-crawler

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,16 +2,41 @@ var Crawler = require("crawler")
 var url = require('url')
 var targetUrl = 'https://github.com/trending/javascript?since=weekly'
 
+var repoUrls = []
+var titleArray = []
+var httpGitUrlArray = []
+
 var c = new Crawler({
   maxConnections: 5,
-  // rateLimit:2000,
-  callback : function (error, res, done) {
+  callback: function (error, res, done) {
+    if(error){
+      console.error(error);
+    }else{
+      var $ = res.$
+      console.log(this)
+
+      // 仓库页面的逻辑处理
+      var httpGitUrl = $('.https-clone-options .js-url-field').attr('value')
+      console.log(httpGitUrl)
+
+      // 保存到数组中
+      httpGitUrlArray.push({
+        url: this.uri,
+        httpGitUrl: httpGitUrl
+      })
+    }
+    console.log(httpGitUrlArray)
+    done();
+  }
+});
+
+c.queue({
+  uri: targetUrl,
+  callback: function (error, res, done) {
     if(error){
       console.error(error);
     }else{
       var $ = res.$;
-      console.log($('.repo-list').length);
-      var repoUrls = [];
       console.log($('.repo-list .mb-1 a').length) // 25
       $('.repo-list .mb-1 a').each(function (index, element) {
         var $element = $(element)
@@ -26,10 +51,9 @@ var c = new Crawler({
         titleArray.push($title)
       })
     }
+    
+    // 直接抓取每个仓库页面
+    c.queue(repoUrls)
     done();
   }
-});
-
-c.queue(targetUrl);
-
-c.queue(repoUrls)
+})
