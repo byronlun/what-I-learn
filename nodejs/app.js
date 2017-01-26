46c6340363317bc938ad6fa16a393da97d47cf7b
byronlun <2664181855@qq.com>
2017-01-26 22:01:45 +0800

    delete annotations

--- nodejs/app.js
+++ nodejs/app.js
@@ -90,17 +90,3 @@ superagent.get(targetUrl)
     }
   )
 })
-
-// async function insertData(person){
-//     let db, collection, result; 
-//     try{
-//         db = await mongoDb.open();
-//         collection = await db.collection("users");
-//         result = await collection.insert(person);
-//     }catch(e){
-//         console.error(e.message);
-//     }
-//     console.log(result);
-// } 
-
-// insertData({name: "yika"});
